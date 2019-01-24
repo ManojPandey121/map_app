b9384275dedfd1fdf9d0fec917d3358fc6c21077
Manoj Pandey <me.nose121@gmail.com>
2019-01-24 10:20:47 +0545

    Map view in flutter

---
+++ lib/main.dart
@@ -0,0 +1,394 @@
+import 'dart:async';
+import 'package:google_maps_webservice/places.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart' as LocationManager;
+import 'place_detail.dart';
+
+
+const kGoogleApiKey = "Your_Api_Key";
+GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: kGoogleApiKey);
+
+void main() {
+  runApp(MaterialApp(
+    title: "PlaceZ",
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  final homeScaffoldKey = GlobalKey<ScaffoldState>();
+  GoogleMapController mapController;
+  List<PlacesSearchResult> places = [];
+  bool isLoading = false;
+  String errorMessage;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget expandedChild;
+    if (isLoading) {
+      expandedChild = Center(child: CircularProgressIndicator(value: null));
+    } else if (errorMessage != null) {
+      expandedChild = Center(
+        child: Text(errorMessage),
+      );
+    } else {
+      expandedChild = buildPlacesList();
+    }
+
+    return Scaffold(
+        key: homeScaffoldKey,
+        appBar: AppBar(
+          title: const Text("PlaceZ"),
+          actions: <Widget>[
+            isLoading
+                ? IconButton(
+              icon: Icon(Icons.timer),
+              onPressed: () {},
+            )
+                : IconButton(
+              icon: Icon(Icons.refresh),
+              onPressed: () {
+                refresh();
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                _handlePressButton();
+              },
+            ),
+          ],
+        ),
+        body: Column(
+          children: <Widget>[
+            Container(
+              child: SizedBox(
+                  height: 200.0,
+                  child: GoogleMap(
+                      onMapCreated: _onMapCreated,
+                      options: GoogleMapOptions(
+                          myLocationEnabled: true,
+                          cameraPosition:
+                          const CameraPosition(target: LatLng(0.0, 0.0))))),
+            ),
+            Expanded(child: expandedChild)
+          ],
+        ));
+  }
+
+  void refresh() async {
+    final center = await getUserLocation();
+
+    mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: center == null ? LatLng(0, 0) : center, zoom: 15.0)));
+    getNearbyPlaces(center);
+  }
+
+  void _onMapCreated(GoogleMapController controller) async {
+    mapController = controller;
+    refresh();
+  }
+
+  Future<LatLng> getUserLocation() async {
+    var currentLocation = <String, double>{};
+    final location = LocationManager.Location();
+    try {
+      currentLocation = await location.getLocation();
+      final lat = currentLocation["latitude"];
+      final lng = currentLocation["longitude"];
+      final center = LatLng(lat, lng);
+      print(center);// -----------------------------------printing lat and long
+
+
+      return center;
+    } on Exception {
+      currentLocation = null;
+      return null;
+    }
+  }
+
+  void getNearbyPlaces(LatLng center) async {
+    setState(() {
+      this.isLoading = true;
+      this.errorMessage = null;
+    });
+
+    final location = Location(center.latitude, center.longitude);
+    final result = await _places.searchNearbyWithRadius(location, 2500);
+    setState(() {
+      this.isLoading = false;
+      if (result.status == "OK") {
+        this.places = result.results;
+        result.results.forEach((f) {
+          final markerOptions = MarkerOptions(
+              position:
+              LatLng(f.geometry.location.lat, f.geometry.location.lng),
+              infoWindowText: InfoWindowText("${f.name}", "${f.types?.first}"));
+          mapController.addMarker(markerOptions);
+        });
+      } else {
+        this.errorMessage = result.errorMessage;
+      }
+    });
+  }
+
+  void onError(PlacesAutocompleteResponse response) {
+    homeScaffoldKey.currentState.showSnackBar(
+      SnackBar(content: Text(response.errorMessage)),
+    );
+  }
+
+  Future<void> _handlePressButton() async {
+    try {
+      final center = await getUserLocation();
+      Prediction p = await PlacesAutocomplete.show(
+          context: context,
+          strictbounds: center == null ? false : true,
+          apiKey: kGoogleApiKey,
+          onError: onError,
+          mode: Mode.fullscreen,
+          language: "en",
+          location: center == null
+              ? null
+              : Location(center.latitude, center.longitude),
+          radius: center == null ? null : 10000);
+
+      showDetailPlace(p.placeId);
+    } catch (e) {
+      return;
+    }
+  }
+
+  Future<Null> showDetailPlace(String placeId) async {
+    if (placeId != null) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => PlaceDetailWidget(placeId)),
+      );
+    }
+  }
+
+  ListView buildPlacesList() {
+    final placesWidget = places.map((f) {
+      List<Widget> list = [
+        Padding(
+          padding: EdgeInsets.only(bottom: 4.0),
+          child: Text(
+            f.name,
+            style: Theme.of(context).textTheme.subtitle,
+          ),
+        )
+      ];
+      if (f.formattedAddress != null) {
+        list.add(Padding(
+          padding: EdgeInsets.only(bottom: 2.0),
+          child: Text(
+            f.formattedAddress,
+            style: Theme.of(context).textTheme.subtitle,
+          ),
+        ));
+      }
+
+      if (f.vicinity != null) {
+        list.add(Padding(
+          padding: EdgeInsets.only(bottom: 2.0),
+          child: Text(
+            f.vicinity,
+            style: Theme.of(context).textTheme.body1,
+          ),
+        ));
+      }
+
+      if (f.types?.first != null) {
+        list.add(Padding(
+          padding: EdgeInsets.only(bottom: 2.0),
+          child: Text(
+            f.types.first,
+            style: Theme.of(context).textTheme.caption,
+          ),
+        ));
+      }
+
+      return Padding(
+        padding: EdgeInsets.only(top: 4.0, bottom: 4.0, left: 8.0, right: 8.0),
+        child: Card(
+          child: InkWell(
+            onTap: () {
+              showDetailPlace(f.placeId);
+            },
+            highlightColor: Colors.lightBlueAccent,
+            splashColor: Colors.red,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: list,
+              ),
+            ),
+          ),
+        ),
+      );
+    }).toList();
+
+    return ListView(shrinkWrap: true, children: placesWidget);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+
+import 'package:location/location.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+
+
+
+
+
+var api_key = "AIzaSyDrHKl8IxB4cGXIoELXQOzzZwiH1xtsRf4";
+void main() {
+  MapView.setApiKey(api_key);
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: new MapPage(),
+  ));
+}
+
+class MapPage extends StatefulWidget {
+  @override
+  _MapPageState createState() => new _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  MapView mapView = new MapView();
+  CameraPosition cameraPosition;
+  var staticMapProvider = new StaticMapProvider(api_key);
+  Uri staticMapUri;
+
+  List<Marker> markers = <Marker>[
+    new Marker("1", "BSR Restuarant", 28.421364, 77.333804,
+        color: Colors.amber),
+    new Marker("2", "Flutter Institute", 28.418684, 77.340417,
+        color: Colors.purple),
+  ];
+
+  showMap() {
+    mapView.show(new MapOptions(
+        mapViewType: MapViewType.normal,
+        initialCameraPosition:
+        new CameraPosition(new Location(28.420035, 77.337628), 15.0),
+        showUserLocation: true,
+        title: "Recent Location"));
+    mapView.setMarkers(markers);
+    mapView.zoomToFit(padding: 100);
+
+    mapView.onMapTapped.listen((_) {
+      setState(() {
+        mapView.setMarkers(markers);
+        mapView.zoomToFit(padding: 100);
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    cameraPosition =
+    new CameraPosition(new Location(28.420035, 77.337628), 2.0);
+    staticMapUri = staticMapProvider.getStaticUri(
+        new Location(28.420035, 77.337628), 12,
+        height: 400, width: 900, mapType: StaticMapViewType.roadmap);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Google maps"),
+      ),
+      body: new Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            height: 300.0,
+            child: new Stack(
+              children: <Widget>[
+                new Center(
+                  child: Container(
+                    child: new Text(
+                      "Map should show here",
+                      textAlign: TextAlign.center,
+                    ),
+                    padding: const EdgeInsets.all(20.0),
+                  ),
+                ),
+                new InkWell(
+                  child: new Center(
+                    child: new Image.network(staticMapUri.toString()),
+                  ),
+                  onTap: showMap,
+                )
+              ],
+            ),
+          ),
+          new Container(
+            padding: new EdgeInsets.only(top: 10.0),
+            child: new Text(
+              "Tap the map to interact",
+              style: new TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          new Container(
+            padding: new EdgeInsets.only(top: 25.0),
+            child: new Text(
+                "Camera Position: \n\nLat: ${cameraPosition.center.latitude}\n\nLng:${cameraPosition.center.longitude}\n\nZoom: ${cameraPosition.zoom}"),
+          ),
+        ],
+      ),
+    );
+  }
+}*/
